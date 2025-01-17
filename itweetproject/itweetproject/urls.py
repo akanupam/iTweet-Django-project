b4751ea8be99b9330a77864557df4e47b8a928a4
Aditya Kumar Anupam <aditanupam@gmail.com>
2025-01-17 23:50:03 +0530

    Second commit for home page

--- itweetproject/itweetproject/urls.py
+++ itweetproject/itweetproject/urls.py
@@ -22,6 +22,7 @@ from django.contrib.auth.urls import views as  auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('TWEET.urls')),
     path('tweet/', include('TWEET.urls')),
     path('accounts/', include('django.contrib.auth.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
